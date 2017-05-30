8a5a7362faf97d130f8d1f78c8403fc9b50ac36c
Ömral Cörüt <omralcrt@gmail.com>
2017-05-30 16:44:41 +0300

    Design other bottom navigation fragments

--- app/src/main/java/com/ovidos/docket/fragments/SubscriptionsFragment.java
+++ app/src/main/java/com/ovidos/docket/fragments/SubscriptionsFragment.java
@@ -1,5 +1,12 @@
 package com.ovidos.docket.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ovidos.docket.R;
 import com.ovidos.docket.core.fragment.BaseFragment;
 
 /**
@@ -10,5 +17,18 @@ public class SubscriptionsFragment extends BaseFragment{
 
     public static SubscriptionsFragment newInstance() {
         return new SubscriptionsFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_subscriptions, container, false);
+
+        return view;
     }
 }
--- app/src/main/java/com/ovidos/docket/fragments/VideosFragment.java
+++ app/src/main/java/com/ovidos/docket/fragments/VideosFragment.java
@@ -1,5 +1,12 @@
 package com.ovidos.docket.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ovidos.docket.R;
 import com.ovidos.docket.core.fragment.BaseFragment;
 
 /**
@@ -10,5 +17,18 @@ public class VideosFragment extends BaseFragment {
 
     public static VideosFragment newInstance() {
         return new VideosFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_videos, container, false);
+
+        return view;
     }
 }
